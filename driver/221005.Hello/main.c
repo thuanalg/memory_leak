b21261fa7da4dab596162a4091916af79a6cc208
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2022-10-07 15:47:53 -0400

    driver

---
+++ driver/221005.Hello/main.c
@@ -0,0 +1,7 @@
+//Ref: https://www.youtube.com/watch?v=4tgluSJDA_E
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	return 0; 
+}
+//https://github.com/Johannes4Linux/Linux_Driver_Tutorial
