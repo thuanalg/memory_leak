aa5718db9f41af7fbcf2da81aa8b8f687c9ac43a
thuannguyen <thuannguyen@localhost.localdomain>
2022-12-06 06:08:11 -0500

    syslog

--- check_leak/main.c
+++ check_leak/main.c
@@ -1,18 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include <syslog.h>
 
-void LOG(void *p) {
-	//Will embed rsyslog later
-	fprintf(stdout, "p:%p\n", p);
-}
-
-#define MY_MALLOC(p, n) {p=malloc(n);LOG(p);}
-#define MY_FREE(p) {free(p);LOG(p);}
+#define MY_MALLOC(p, n) {p=malloc(n);syslog(LOG_ERR, "line: %d, malloc p: %p, n: %d\n", __LINE__, p, n); }
+#define MY_FREE(p) {free(p);syslog(LOG_ERR, "line: %d, free p: %p\n", __LINE__, p);}
 
 
 int main(int argc, char *argv) {
 	char *str = 0;
+	setlogmask (LOG_UPTO (LOG_NOTICE));
+	openlog ("memoryyyy_leak", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 	MY_MALLOC(str, 10)
 	MY_FREE(str);
+	closelog ();
 	return 0;
 }
+
+
+//https://www.gnu.org/software/libc/manual/html_node/Syslog-Example.html
