ceb17bf2b298abcdc292de65af21854e3d4fb530
thuannguyen <thuannguyen@localhost.localdomain>
2023-07-27 11:51:43 +0700

    en

--- math/main_combine.cpp
+++ math/main_combine.cpp
@@ -8,11 +8,13 @@
 #include <iostream>
 
 using namespace std;
+#define ULL         unsigned long long
 
 unsigned long long factorial(int n);
 void fdeg(int n, char *c, char *out);
 unsigned long long combination(int n, int k);
 void fprbinomial(int n, std::string &str);
+void pascal_triangle(int n, ULL **out);
 
 std::string formula(int n) {
 	std::string str = "";
@@ -33,10 +35,12 @@ unsigned long long combination(int n, int k) {
 }
 void fprbinomial(int degg, std::string& str) {
     //int n = 0;
+    ULL *pascal = 0;
     int deg = degg;
     if(degg < 0) {
         deg = -degg;
     }
+    pascal_triangle(deg, &pascal);
     do {
         if(deg == 0) {
             str = "1";
@@ -68,11 +72,13 @@ void fprbinomial(int degg, std::string& str) {
                     fdeg(deg - i, "a", fdega);
                     fdeg(i, "b", fdegb);
 
-                    snprintf(tmp, 100, "%d%s%s+", combination(deg, i), fdega, fdegb);
+                    //snprintf(tmp, 100, "%d%s%s+", combination(deg, i), fdega, fdegb);
+                    snprintf(tmp, 100, "%llu%s%s+", pascal[i], fdega, fdegb);
                     str += tmp;                    
                 }                
                 else if(i < deg) {
-                    snprintf(tmp, 100, "%da^%db^%d+", combination(deg, i), deg - i, i);
+                    //snprintf(tmp, 100, "%da^%db^%d+", combination(deg, i), deg - i, i);
+                    snprintf(tmp, 100, "%llua^%db^%d+", pascal[i], deg - i, i);
                     str += tmp;
                 }
                 else if(i == deg) {
@@ -88,6 +94,7 @@ void fprbinomial(int degg, std::string& str) {
         str.insert(0, "1/(");
         str += ")";
     }
+    free(pascal);
 }
 void fdeg(int n, char *c, char *out){
     if(n == 0) {
@@ -101,6 +108,44 @@ void fdeg(int n, char *c, char *out){
 }
 
 
+void pascal_triangle(int n, ULL **out) {
+    if(n == 0 || n == 1) {
+        *out = (ULL *) malloc(2 * sizeof(ULL));
+        (*out)[0] = 1;
+        (*out)[1] = 1;
+    }
+    else if (n == 2) {
+        *out = (ULL *) malloc(4 * sizeof(ULL));
+        (*out)[0] = 1;
+        (*out)[1] = 2;
+        (*out)[2] = 1;
+    } 
+    else {
+        ULL *arrB = 0;
+        pascal_triangle(n - 1, &arrB);
+        *out = (ULL *) malloc( (n * 2) * sizeof(ULL));
+        memset(*out, 0, (n * 2) * sizeof(ULL));
+        (*out)[0] = 1;
+        (*out)[n] = 1;
+        if(n % 2) {//3, 5, 7, 9
+            for(int i = 1; i < (n+1)/2; ++i) {
+                (*out)[i] = arrB[i-1] + arrB[i];
+                (*out)[n-i] = (*out)[i];
+            }
+        } else {//2, 4, 6, 8, 10
+            for(int i = 1; i <= (n)/2; ++i) {
+                if( i%2 != 0 && (2 * i) >= n) {
+                    (*out)[i] = 2 * arrB[i-1];
+                } else {
+                    (*out)[i] = arrB[i-1] + arrB[i];                   
+                }
+                (*out)[n-i] = (*out)[i];
+            }       
+        }
+
+        free(arrB);
+    }
+}
 
 
 
@@ -112,6 +157,7 @@ int main(int argc, char *argv[]) {
     int n = 0;
     int k = 0;
     int deg = 0;
+    ULL *pascal = 0;
     std::string str;
     sscanf(argv[1], "%d", &n);
     sscanf(argv[2], "%d", &k);
@@ -119,5 +165,11 @@ int main(int argc, char *argv[]) {
     fprintf(stdout, "combination(%d, %d) = %d\n", n, k, combination(n,k));
     str = formula(deg);
     fprintf(stdout, "---%s---\n\n", str.c_str());
+    pascal_triangle(deg, &pascal);
+    for(int i = 0; i <= 2*n + 1 || pascal[i] > 0; ++i) {
+        if(!pascal[i]) break;
+        fprintf(stdout, "%llu ", pascal[i]);
+    }
+    free(pascal);
     return 0;
 }
