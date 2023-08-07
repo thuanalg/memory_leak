d7a3397696096f26022dd6dff8d1b325c4eed9f4
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2023-08-07 10:36:26 +0700

    en

--- poker/main.c
+++ poker/main.c
@@ -3,8 +3,13 @@
 #include <string.h>
 #include <stdio.h>
 
+#define SEVEN 7
+//https://vi.wikipedia.org/wiki/Texas_hold_%27em
+typedef struct {
+	char kind;
+	char cards[SEVEN + 1];
+} POKER;
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 	return EXIT_SUCCESS;
 }
