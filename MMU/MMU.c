9f69d20f4c06d2d6bcf95f9778687e6dc9d666d1
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2023-08-29 09:28:22 +0700

    en

--- MMU/MMU.c
+++ MMU/MMU.c
@@ -1,7 +1,7 @@
-#include <string.h>
+
 #include <stdio.h>
 
-#define BUFF_SIZE 8  //A segment
+#define BUFF_SIZE 32  //A segment
 #define MAX_BLOCK 20    //Maximum of number of pointers are allocated.
 #define CAST_PCHAR(p)   ((char*)(p))
 #define myfprintf(fm, ...) fprintf(stdout, "\nline: %d, "fm"\n", __LINE__, ##__VA_ARGS__)
@@ -47,6 +47,8 @@ int main(int argc, char *argv[]) {
     p = mymalloc(BUFF_SIZE - 1);
     myfprintf("p: %p\n", p);
 
+    dump_list();
+
     p1 = mymalloc(1);
     myfprintf("p1: %p\n", p1);
     dump_list();
@@ -56,29 +58,36 @@ int main(int argc, char *argv[]) {
     myfprintf("p1: %p\n", p1);
     dump_list();
 
-    p1 = mymalloc(1);
+    p1 = mymalloc(16);
     myfprintf("p1: %p\n", p1);
-
+//
     p3 = mymalloc(3);
     myfprintf("p3: %p\n", p3);
-
+//
     dump_list();
-
+//
     myfree(p3);
-
+//
     p1 = mymalloc(4);
     myfprintf("p1: %p\n", p1);
     dump_list();
-
+//
     p1 = mymalloc(2);
     myfprintf("p1: %p\n", p1);
     dump_list();
-
+//
     p1 = mymalloc(1);
     myfprintf("p1: %p\n", p1);
     dump_list();
-
+//
     myfree(p1);
+    dump_list();
+
+    p1 = mymalloc(10);
+    dump_list();
+
+    p1 = mymalloc(1);
+    p1 = mymalloc(1);
     dump_list();
 
     try_clear();
@@ -87,7 +96,6 @@ int main(int argc, char *argv[]) {
 void *mymalloc(int n) {
     //Non multi-thread here, not thread-safe
     void *p = 0;
-    item_block mem;
     int k = 0;
     int m = STACK_ITEMS(stack);
     item_block *t = (item_block *)stack->data;   
@@ -103,7 +111,7 @@ void *mymalloc(int n) {
 
         if(m < 1) {
             p = mysegment;
-            myfprintf("--- option 0") ;  
+            myfprintf("--- option 0 new") ;  
             break;
         } 
         
@@ -112,14 +120,14 @@ void *mymalloc(int n) {
             if (CAST_PCHAR(t[0].p) -  CAST_PCHAR(mysegment) >= n) {
                 p = mysegment;
                 //k = 0;  
-                myfprintf("--- option 1") ;  
+                myfprintf("--- option 1 insert at the first") ;  
                 break;          
             }
 
             while (k < (m -1)) {
                 if(CAST_PCHAR(t[k + 1].p) -  (CAST_PCHAR(t[k].p) + t[k].n) >= (n)) {
                     p = CAST_PCHAR(t[k].p) + t[k].n;
-                    myfprintf("--- option 2") ;  
+                    myfprintf("--- option 2 insert at middle") ;  
                     ++k;
                     break;
                 }
@@ -130,7 +138,7 @@ void *mymalloc(int n) {
             {
                 p = CAST_PCHAR(t[m-1].p) + t[m-1].n;
                 k = m;         
-                myfprintf("--- option 3") ;  
+                myfprintf("--- option 3 Append, p: %p, len: %d", CAST_PCHAR(t[m-1].p), t[m-1].n) ;  
                 break;          
             }
         }
@@ -138,6 +146,7 @@ void *mymalloc(int n) {
     } while(0);
     if(p) {
         //insert  INTO MMU STACK AT k position
+        item_block mem;
         mem.n = n;
         mem.p = p;
         myinsert(stack, CAST_PCHAR(&mem), k);
@@ -153,7 +162,11 @@ void myfree(void *p) {
     for(i = 0; i < n; ++i) {
         if(p == t[i].p) {   
             if(i < n -1) {
-                memcpy(t + i, t + i + 1, sizeof(item_block) * (n - i - 1));
+                //memcpy(t + i, t + i + 1, sizeof(item_block) * (n - i - 1));
+                int j = 0;
+                for(j = i; j < (n - 1); ++j) {
+                    t[j] = t[j + 1];
+                }
             }
             t[n-1].n = 0;
             stack->used -= (int)sizeof(item_block);
@@ -169,13 +182,18 @@ void myinsert(mmu_list *s, char *data, int i) {
     item_block *t = (item_block *)stack->data;
     do {
         if(n < 1) {
-            memcpy(t, data, sizeof(item_block));  
+            //memcpy(t, data, sizeof(item_block));  
+            t[0] = *((item_block *)data);
             break;
         }
         myfprintf("src: %p, dest: %p, sz: %d", 
             CAST_PCHAR(t + i), CAST_PCHAR(t + i + 1), (n - i) * sizeof(item_block));
-        memmove(CAST_PCHAR(t + i + 1), CAST_PCHAR(t + i), (n - i) * sizeof(item_block));
-        memcpy(CAST_PCHAR(t + i), data, sizeof(item_block)); 
+        //memmove(CAST_PCHAR(t + i + 1), CAST_PCHAR(t + i), (n - i) * sizeof(item_block));
+        for(int j = n; j > i; j-- ) {
+            t[j] = t[j -1];
+        }
+        //memcpy(CAST_PCHAR(t + i), data, sizeof(item_block)); 
+        t[i] = *((item_block *)data);
     } while(0);
     stack->used += sizeof(item_block);
 }
@@ -192,11 +210,7 @@ void try_clear() {
     item_block *t = (item_block *)stack->data; 
     int n = STACK_ITEMS(stack);
     while(n) {
-        if(n > 1) {
-            myfree(t[n/2].p);
-        } else {
-            myfree(t[0].p);
-        }
+        myfree(t[n/2].p);
         n = STACK_ITEMS(stack);
         dump_list();
     }
