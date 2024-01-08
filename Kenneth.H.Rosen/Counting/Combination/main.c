0d7c90459d330c7658a554d80bafe914d8351bfd
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2024-01-08 21:19:29 +0700

    en

---
+++ Kenneth.H.Rosen/Counting/Combination/main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define uint unsigned int
+int get_next_rcom(uint* arr, uint n);
+int get_next_rcom(uint* arr, uint n) {
+	return 0;
+}
+int main(int argc, char* argv[]) {
+	return 0;
+}
