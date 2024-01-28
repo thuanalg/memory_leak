19c6de79c5c35ddb2bf551cfcb4c5bd437d80c6b
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2024-01-28 08:55:26 +0700

    en

--- Kenneth.H.Rosen/Counting/n_permutation/main.c
+++ Kenneth.H.Rosen/Counting/n_permutation/main.c
@@ -1,125 +1,112 @@
 //Nguyen Thai Thuan, modify later
 #include <stdlib.h>
 #include <stdio.h>
-#define MY_MALLOC(k, btype, obj, objtype) {obj = (objtype *) malloc(k * sizeof(btype) + sizeof(uint)); if(!obj) { llog("malloc error.\n");exit(1);} else {memset(obj, 0, (k * sizeof(btype) + sizeof(uint))); obj->n = (k * sizeof(btype) + sizeof(uint));}}
+#include <string.h>
+#define MY_MALLOC(k, btype, obj, objtype) {obj = (objtype *) malloc(k * sizeof(btype) + sizeof(int)); if(!obj) { llog("malloc error.\n");exit(1);} else {memset(obj, 0, (k * sizeof(btype) + sizeof(int))); obj->n = (k * sizeof(btype) + sizeof(int));}}
 #define llog(fm, ...) fprintf(stdout, "%s:%d >>> "fm"", __FILE__, __LINE__, ##__VA_ARGS__)
-#define uint		unsigned int
-#define uchar		unsigned char
+//#define uint		unsigned int
+//#define uchar		unsigned char
+#define swap(A, B, tmp) {(tmp) = (A); (A) = (B); (B) = (tmp);}
 typedef enum MY_ROLE__ {
 	STRAIGHT = 0,
 	HOLE_GAP = 1,
 
 } MY_ROLE;
 typedef struct __My_GENERIC__ {
-	uint n;
+	int n;
 	char data[0];
 } My_GENERIC;
 typedef struct __My_bit_COUNT__ {
-	uint n;
+	int n;
 	My_GENERIC* arr;
 } My_bit_COUNT;
-#define combination_st  My_bit_COUNT
-int init_rcom(combination_st* rcom, uint n, uint r);
-int init_rcom(combination_st* rcom, uint n, uint r) {
+#define permutation_st  My_bit_COUNT
+int init_rpermu(permutation_st* rcom, int n);
+int init_rpermu(permutation_st* rcom, int n) {
 	int err = 0;
 	My_GENERIC* p = 0;
 	do {
 		if (!rcom) {
 			err = 1; break;
 		}
-		memset(rcom, 0, sizeof(combination_st));
+		memset(rcom, 0, sizeof(permutation_st));
 		rcom->n = n;
-		rcom->r = r;
-		rcom->b = 0;
-		rcom->e = rcom->r - 1;
-		MY_MALLOC((rcom->n), char, p, My_GENERIC);
+		MY_MALLOC((rcom->n), int, p, My_GENERIC);
 		rcom->arr = p;
-		char* arr = rcom->arr->data;
-		for (int i = rcom->b; i <= rcom->e; ++i) {
-			arr[i] = 1;
+		int* arr = (int *)rcom->arr->data;
+		for (int i = 0; i < rcom->n; ++i) {
+			arr[i] = i;
 		}
 	} while (0);
 	return err;
 }
 
 int countt = 1;
-int get_next_rcom(combination_st* p);
-void dum_value(combination_st* comr);
+int get_next_rpermu(permutation_st* p);
+void dum_value(permutation_st* comr);
 int main(int argc, char* argv[]) {
 	int next = 1;
-	combination_st rcom;
-	init_rcom(&rcom, 6, 3);
+	permutation_st rcom;
+	init_rpermu(&rcom, 4);
 	while (next) {
+			
 		dum_value(&rcom);
-		next = get_next_rcom(&rcom);
+		next = get_next_rpermu(&rcom);
 	}
 	return 0;
 }
 
 
-void dum_value(combination_st* comr) {
-	uint i = comr->b;
-	char* p = comr->arr->data;
+void dum_value(permutation_st* permu) {
+	int i = 0;
+	int* p = (int*) permu->arr->data;
 	fprintf(stdout, "\ncount : %d====\t", countt);
 	++countt;
-	for (; i <= comr->e; ++i) {
-		if (p[i]) {
-			fprintf(stdout, "%u, ", i);
-		}
+	for (i = 0; i < permu->n; ++i) {
+		fprintf(stdout, "%u, ", p[i]);
 	}
 	fprintf(stdout, "\n");
 }
-int get_next_rcom(combination_st* comr) {
+int get_next_rpermu(permutation_st* permu) {
 	MY_ROLE mode = STRAIGHT;
-	uint i = 0;
-	uint j = 0;
-	int next = 1;
-	char* p = comr->arr->data;
-	for (i = comr->b; i <= comr->e; ++i) {
-		if (!p[i]) {
-			j = i;
-			mode = HOLE_GAP;
-			break;
-		}
-	}
+	int i = 0;
+	int next = 0;
+	int* p = (int *) permu->arr->data;
+	int tmp = 0;
 	do {
-		if (mode == STRAIGHT && (comr->e >= comr->n - 1)) {
-			next = 0;
-			break;
-		}
-		if (mode == STRAIGHT) {
-			for (i = 0; i < comr->n; ++i) {
-				p[i] = 0;
-			}
-			comr->b = 0;
-			++(comr->e);
-			p[comr->b] = 0;
-			for (i = 0; i < comr->r - 1; ++i) {
-				p[i] = 1;
-			}
-			p[comr->e] = 1;
-			break;
-		}
-		if (mode == HOLE_GAP) {
-			p[i] = 1;
-			p[i - 1] = 0;
-			if (i - 1 == comr->b) {
-				comr->b = i;
-			}
-			else if (i - 1 > comr->b) {
-				uint range = i - 1 - comr->b;
-				uint j = 0;
-				uint  k = i - 1 - comr->b;
-				for (j = 0; j < range; ++j) {
-					p[j] = 1;
+		int t = 0;
+		int u = 0;
+		int v = 0;
+		do {
+			t = -1;
+			u = -1;
+			v = -1;
+			for (i = permu->n - 1; i > 0; i--) {
+				if (p[i] > p[i - 1]) {
+					t = i - 1;
+					u = i;
+					v = permu->n - 1;
+					break;
 				}
-				for (j = range; j <= i - 1; ++j) {
-					p[j] = 0;
-				}
-				comr->b = 0;
 			}
-			break;
-		}
+			if (t < 0) {
+				break;
+			}
+			//Swap 
+			for (i = permu->n - 1; i > 0; i--) {
+				if (p[i] > p[t]) {
+					swap(p[i], p[t], tmp)
+					break;
+				}
+			}
+			//Reverse
+			i = 0;
+			while ((u + i) < (v - i)) {
+				swap(p[u + i], p[v - i], tmp);
+				++i;
+			}
+			next = 1;
+		} while (0);
 	} while (0);
 
 	return next;
