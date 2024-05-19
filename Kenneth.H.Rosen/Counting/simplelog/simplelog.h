e6059af4f8d99971a2769fe4de912201b834ab80
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2024-05-19 11:42:27 +0700

    en

--- Kenneth.H.Rosen/Counting/simplelog/simplelog.h
+++ Kenneth.H.Rosen/Counting/simplelog/simplelog.h
@@ -17,9 +17,18 @@ extern "C" {
 #define		LOG_WARNING				30
 #define		LOG_ERROR				50
 #define		LOG_FATAL				100
+	typedef struct __SIMPLE_LOG_ST__ {
+		char filepath[1024];
+		void* fp;
+		int szbuf;
+		void *mtx;
+		void* sem_trigger;
+		char off;
+	} SIMPLE_LOG_ST;
 	extern int			simple_log_levwel;
 	DLL_API_SIMPLE_LOG int			simple_set_log_levwel(int val);
 	DLL_API_SIMPLE_LOG int			simple_get_log_levwel();
+	DLL_API_SIMPLE_LOG int			simple_init_log(int lvel, char *path);
 
 #ifdef __cplusplus
 }
