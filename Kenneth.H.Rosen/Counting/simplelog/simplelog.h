307a116fc6a1da1524361200834c70a01570f244
Thuan T. Nguyen <nguyenthaithuanalg@gmail.com>
2024-06-16 17:09:38 +0700

    en

--- Kenneth.H.Rosen/Counting/simplelog/simplelog.h
+++ Kenneth.H.Rosen/Counting/simplelog/simplelog.h
@@ -48,6 +48,7 @@ extern "C" {
 #define consimplelog(___fmttt___, ...)		fprintf(stdout, "[WIN32_MSVC] "__FILE_LINE_SIMPLELOG__" "___fmttt___"\n" ,__FILE__, __LINE__, ##__VA_ARGS__)
 #define consimplelog_buffer(buuf__, _n_n, ___fmttt___, ...)		snprintf((buuf__), (_n_n), __SIMPLE_LOG_PLATFORM__" "__FILE_LINE_SIMPLELOG__" "___fmttt___"\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
+
 #define		LOG_DEBUG				0
 #define		LOG_INFO				70
 #define		LOG_WARNING				80
@@ -77,7 +78,6 @@ extern "C" {
 	DLL_API_SIMPLE_LOG int			simple_get_log_levwel();
 	DLL_API_SIMPLE_LOG int			simple_init_log(char *path);
 	DLL_API_SIMPLE_LOG LLU			simple_log_time_now(int* delta);
-	DLL_API_SIMPLE_LOG int			simple_log_name_now(char* name);
 	DLL_API_SIMPLE_LOG int			simple_log_name_now(char* name);
 	DLL_API_SIMPLE_LOG int			simple_log_fmt_now(char* fmtt, int len);
 	DLL_API_SIMPLE_LOG int			spl_mutex_lock(void* mtx);
